ca24fa24659591aa9f4ca4faacc79ff73bbf44d9
Alexandr Ivashkov <mini1789@yandex.ru>
2024-08-21 16:21:35 +0300

    День 12 работа с моделями и выборками созданы новые методы в DevController

--- app/Http/Controllers/DevController.php
+++ app/Http/Controllers/DevController.php
@@ -2,7 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Project;
+use App\Models\User;
+use Illuminate\Database\Query\JoinClause;
 use Illuminate\Http\Request;
+use DB;
 
 class DevController extends Controller
 {
@@ -29,5 +33,65 @@ class DevController extends Controller
     public function getDummyConfig()
     {
         return config('dummyjson');
+    }
+
+    /*
+     * Создаем 5 случайных проектов
+     */
+    public static function addProject()
+    {
+        for ($i = 1; $i <= 5; $i++) {
+            Project::query()->create([
+                'name' => 'Project ' . fake()->domainName(),
+                'owner_id' => User::query()->inRandomOrder()->first()->id,
+                'is_active' => fake()->boolean(),
+                'deadline_date' => fake()->dateTimeThisYear("+120 days"),
+                'assignee_id' => User::query()->inRandomOrder()->first()->id,
+            ]);
+        }
+
+        return '5 projects added';
+    }
+
+    /*
+     * Получаем все проекты админов и присоединяем нужные поля от юзера к выборке
+     */
+    public static function getAdminProjects()
+    {
+        $projects = DB::table('projects')
+            ->join('users', function (JoinClause $join) {
+                $join->on('projects.owner_id', '=', 'users.id')
+                    ->where('users.role', '=', 'admin');
+            })
+            ->select('projects.*', 'users.id as user_id', 'users.username', 'users.email')
+            ->get();
+
+        return $projects;
+    }
+
+    /*
+     * Получаем проекты с истекшим дедлайном сортировка по дате дедлайна ASC
+     */
+    public static function getExpired()
+    {
+        $projects = DB::table('projects')
+            ->where('deadline_date', '<', now())
+            ->orderBy('deadline_date', 'asc')
+            ->get();
+
+        return $projects;
+    }
+
+    public static function updateRandom()
+    {
+        $pogect = Project::query()->inRandomOrder()->first()->update([
+            'name' => 'New Project ' . fake()->domainName(),
+            'owner_id' => User::query()->inRandomOrder()->first()->id,
+            'is_active' => fake()->boolean(),
+            'deadline_date' => fake()->dateTimeThisYear("+120 days"),
+            'assignee_id' => User::query()->inRandomOrder()->first()->id,
+        ]);
+
+        return $pogect;
     }
 }
